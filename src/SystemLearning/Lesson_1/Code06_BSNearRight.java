9be8d97963fad7ca7de58bd42c33263864f77c5a
huangqian <huangqian1985@qq.com>
2021-05-22 00:53:35 +0800

    二分查找习题3 + 对数器 在一个有序数组中，找<=某个数最右侧的位置

--- src/SystemLearning/Lesson_1/Code06_BSNearRight.java
+++ src/SystemLearning/Lesson_1/Code06_BSNearRight.java
@@ -4,37 +4,31 @@ import SystemLearning.Common.ArrayFunction;
 
 import java.util.Arrays;
 
-// 在一个有序数组中，找>=某个数最左侧的位置
+// 在一个有序数组中，找<=某个数最右侧的位置
 // 时间复杂度 O(logN)
-public class Code05_BSNearLeft {
+public class Code06_BSNearRight {
 
     /**
-     *
      * @param arr       有序数组
-     * @param value     >=的某个数
-     * @return          >= value 的最左侧的index
+     * @param value     <=的某个数
+     * @return          <= value 的最右侧的index
      */
     public static int nearestIndex(int[] arr, int value) {
         if (null == arr || arr.length == 0) {
             return -1;
         }
-        // 记录当前 >= 数的index位置, 初始赋值为无效值(数组下标最大值+1)
-        int nearestIndex = -1;
+
         int L = 0;
         int R = arr.length - 1;
+        int nearestIndex = -1;
         int mid = 0;
-        //
         while (L <= R) {
-            // 二分到当前L R的中间
-            mid = L + ( (R - L) >> 1 );
-            // 当前值 >= value, 向左二分
-            if ( arr[mid] >= value) {
-                // 更新需要返回的索引值
+            mid = L + ((R - L) >> 1);
+            if (arr[mid] <= value) {
                 nearestIndex = mid;
-                R = mid - 1;
-            } else {
-                // 否则向右二分
                 L = mid + 1;
+            } else {
+                R = mid - 1;
             }
         }
         return nearestIndex;
@@ -42,8 +36,8 @@ public class Code05_BSNearLeft {
 
     // for test
     public static int test(int[] arr, int value) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= value) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] <= value) {
                 return i;
             }
         }
@@ -51,7 +45,7 @@ public class Code05_BSNearLeft {
     }
 
     public static void main(String[] args) {
-        int testTime = 100000;
+        int testTime = 500000;
         int maxSize = 10;
         int maxValue = 100;
         boolean succeed = true;
