53fcbcb9575749cff1dd893235b31a8addd76b77
huangqian <huangqian1985@qq.com>
2021-06-20 01:03:47 +0800

    Lesson5 归并排序附加题、随机快速排序 习题1 给定一个数组arr，两个整数lower和upper，返回arr中有多少个子数组的累加和在[lower,upper]范围上
    加入前缀和数组的暴力解法, 依然没过Leetcode, 超时了

--- SystemLearning/src/Lesson5/Code01_CountOfRangeSum.java
+++ SystemLearning/src/Lesson5/Code01_CountOfRangeSum.java
@@ -6,7 +6,45 @@ public class Code01_CountOfRangeSum {
 		return 0;
 	}
 
+	// 求前缀和数组
+	public static long[] calcSums(int[] nums) {
+		long sum = 0;
+		long[] sums = new long[nums.length];
+		sums[0] = nums[0];
+
+		for (int i = 1; i < nums.length; i++) {
+			sums[i] = sums[i-1] + nums[i];
+		}
+
+		return sums;
+	}
+
+	//
+	public static int comparator2(int[] nums, int lower, int upper) {
+		if (nums.length==1) {
+			return nums[0] >= lower && nums[0] <= upper ? 1 : 0;
+		}
+		int count = 0;
+		// 获取前缀和数组
+		long[] sums = calcSums(nums);
+		//
+		if (sums[0] >= lower && sums[0] <= upper )
+			count++;
+		//
+		for (int i = 1; i < sums.length; i++) {
+			for (int j = 0; j <= i; j++) {
+				// 原数组从 j 位置开始到 i 位置结束的和 等于 前缀和数组从 0 开始到 i 结尾的值减去 从 0 开始到 j-1 位置的值
+				// sum[j->i] = sum[0->i] - sum[0->(j-1)]
+				long value = j == 0 ? sums[i] : sums[i] - sums[j-1];
+				if (value >= lower && value <= upper)
+					count++;
+			}
+		}
+		return count;
+	}
+
 	// for test
+	// O(N * 三次方)
 	public static int comparator(int[] nums, int lower, int upper) {
 		if (nums.length==1) {
 			return nums[0] >= lower && nums[0] <= upper ? 1 : 0;
@@ -28,6 +66,6 @@ public class Code01_CountOfRangeSum {
 		int[] sum = new int[]{-2147483647,0,-2147483647,2147483647};
 		int lower = -564;
 		int upper = 3864;
-		System.out.println(comparator(sum, lower, upper));
+		System.out.println(comparator2(sum, lower, upper));
 	}
 }
