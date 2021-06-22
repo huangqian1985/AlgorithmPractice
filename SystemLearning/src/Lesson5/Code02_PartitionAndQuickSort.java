556519c54d648bbadb478b462a12994bd70b79d5
huangqian <huangqian1985@qq.com>
2021-06-22 23:41:34 +0800

    Lesson5 归并排序附加题、随机快速排序 习题2
    荷兰国旗问题实现

--- SystemLearning/src/Lesson5/Code02_PartitionAndQuickSort.java
+++ SystemLearning/src/Lesson5/Code02_PartitionAndQuickSort.java
@@ -37,8 +37,48 @@ public class Code02_PartitionAndQuickSort {
 		return lessIndex;
 	}
 
+	// arr[L...R] 玩荷兰国旗问题的划分，以arr[R]做划分值
+	// <arr[R] ==arr[R] > arr[R]
+	//
+	// 返回值为等于区域的索引范围
+	public static int[] netherlandsFlag(int[] arr, int L, int R) {
+		// base case
+		if (L > R)
+			return new int[]{-1, -1};
+		if (L == R)
+			return new int[]{L, R};
+		// < 和 > 区域的边界索引
+		int lessIndex = L - 1;
+		int largeIndex = R;
+		// 当前数索引
+		int curIndex = L;
+		// 把R位置作为目标数字
+		int compareValue = arr[R];
+		// 当前位置没有到 > 区域索引 之前循环
+		while (curIndex < largeIndex) {
+			// 如果当前数 = 目标数, 当前数跳下一个
+			if (arr[curIndex] == compareValue) {
+				curIndex++;
+			}
+			// 如果当前数 < 目标数, 当前数和 < 区域下一个数交换, < 区域向右扩, 当前数跳下一个
+			else if (arr[curIndex] < compareValue) {
+				ArrayFunction.Swap(arr, curIndex++, ++lessIndex );
+			}
+			// 如果当前述 > 目标数, 当前数和 > 区域的前一个交换, > 区域向左扩, 当前数不动(因为交换过来的数并未比较)
+			else {
+				ArrayFunction.Swap(arr, curIndex, --largeIndex );
+			}
+		}
+		// 将目标数, 与 > 区域的第一个数交换, 大于区域向右扩
+		ArrayFunction.Swap(arr, largeIndex, R);
+
+		return new int[]{lessIndex+1, largeIndex};
+	}
+
 
 	public static void main(String[] args) {
-
+		int[] arr = new int[]{0,5,9,2,4,-1,2,4,3,6,8,3};
+		netherlandsFlag(arr, 0, arr.length-1);
+		System.out.println(Arrays.toString(arr));
 	}
 }
